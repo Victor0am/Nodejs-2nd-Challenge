f11ec43d76e986373dbcd812069a113ba91c852f
Victor Aguiar <victoramarques@hotmail.com>
2022-06-06 20:07:30 -0300

    checkCreateTodosUserAvailability

--- src/index.js
+++ src/index.js
@@ -24,8 +24,16 @@ function checksExistsUserAccount(request, response, next) {
   return next();
 }
 
+// Middleware para verificar se o usuário ainda pode cadastrar um todo
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const {user} = request;
+
+  if (user.todos.length >= 10 && !user.pro ) {
+    return response.status(403).json({ error: 'User has reached the maximum number of todos' });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
